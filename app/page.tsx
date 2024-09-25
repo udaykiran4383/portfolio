86877cd8006ccdd5b5b7ca4a93ff602041dc6366
uday <uday@udays-MacBook-Air-2.local>
2024-09-26 01:19:06 +0530

    globe working

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import GridGlobe from "@/components/ui/GridGlobe";
+
+
+
+
+
+const Home = () => {
+  return (
+    <main >
+      <div className=" w-full">
+    <GridGlobe/>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
